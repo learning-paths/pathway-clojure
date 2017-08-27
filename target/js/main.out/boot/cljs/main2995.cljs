82d9da564c779f0f7044c564fbe38744e2e58411
Jay Martin <james.martin7@gmail.com>
2017-08-27 12:17:03 -0400

    update readme

--- target/js/main.out/boot/cljs/main2995.cljs
+++
@@ -1,1 +0,0 @@
-(ns boot.cljs.main2995 (:require site.core))
--- target/js/main.out/boot/cljs/main2995.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.9.473 {}
-goog.provide('boot.cljs.main2995');
-goog.require('cljs.core');
-goog.require('site.core');
-
-//# sourceMappingURL=main2995.js.map
