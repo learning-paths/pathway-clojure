7fecfaec50a4e6317d6b6352c81b80f2d38685f5
Jay Martin <james.martin7@gmail.com>
2017-09-16 14:44:36 -0400

    new build

--- target/js/main.out/cljs_deps.js
+++ target/js/main.out/cljs_deps.js
@@ -1,14 +1,4 @@
 goog.addDependency("base.js", ['goog'], []);
 goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.math.Integer', 'goog.string.StringBuffer', 'goog.array', 'goog.math.Long']);
 goog.addDependency("../site/core.js", ['site.core'], ['cljs.core']);
-goog.addDependency("../clojure/browser/event.js", ['clojure.browser.event'], ['cljs.core', 'goog.events.EventTarget', 'goog.events.EventType', 'goog.events']);
-goog.addDependency("../clojure/browser/net.js", ['clojure.browser.net'], ['goog.net.xpc.CfgFields', 'goog.net.XhrIo', 'goog.json', 'goog.Uri', 'cljs.core', 'goog.net.EventType', 'goog.net.xpc.CrossPageChannel', 'goog.net.WebSocket', 'clojure.browser.event']);
-goog.addDependency("../adzerk/boot_reload/connection.js", ['adzerk.boot_reload.connection'], ['cljs.core', 'clojure.browser.net']);
-goog.addDependency("../clojure/string.js", ['clojure.string'], ['goog.string', 'cljs.core', 'goog.string.StringBuffer']);
-goog.addDependency("../adzerk/boot_reload/reload.js", ['adzerk.boot_reload.reload'], ['goog.net.jsloader', 'goog.Uri', 'cljs.core', 'goog.async.DeferredList', 'clojure.string']);
-goog.addDependency("../adzerk/boot_reload/display.js", ['adzerk.boot_reload.display'], ['adzerk.boot_reload.connection', 'goog.dom', 'goog.Timer', 'cljs.core', 'goog.events.EventType', 'clojure.string', 'goog.events']);
-goog.addDependency("../adzerk/boot_reload/websocket.js", ['adzerk.boot_reload.websocket'], ['cljs.core', 'clojure.browser.net', 'goog.net.WebSocket', 'clojure.browser.event']);
-goog.addDependency("../cljs/reader.js", ['cljs.reader'], ['goog.string', 'cljs.core', 'goog.string.StringBuffer']);
-goog.addDependency("../adzerk/boot_reload/client.js", ['adzerk.boot_reload.client'], ['adzerk.boot_reload.connection', 'adzerk.boot_reload.reload', 'adzerk.boot_reload.display', 'goog.net.jsloader', 'goog.Uri', 'cljs.core', 'adzerk.boot_reload.websocket', 'clojure.browser.net', 'cljs.reader', 'clojure.browser.event']);
-goog.addDependency("../adzerk/boot_reload/js/main.js", ['adzerk.boot_reload.js.main'], ['cljs.core', 'adzerk.boot_reload.client']);
-goog.addDependency("../boot/cljs/main2968.js", ['boot.cljs.main2968'], ['cljs.core', 'site.core', 'adzerk.boot_reload.js.main']);
+goog.addDependency("../boot/cljs/main2965.js", ['boot.cljs.main2965'], ['cljs.core', 'site.core']);
